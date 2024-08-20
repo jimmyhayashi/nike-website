0159b913d3f9ed880d5a2a9d5db6138d1a5f8768
Jimmy Hayashihara <jimmyhayashi@live.com>
2024-08-20 19:44:55 +0200

    Setup Vite and Tailwind + download of files

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="border-red-400 relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Services
+}
